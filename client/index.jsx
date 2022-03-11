001cfab0aadf8d1f9166ece35608ab3a4db59922
Christian Gregersen <chgr007@student.kristiania.no>
2022-03-11 15:39:00 +0100

    changes

--- client/index.jsx
+++ client/index.jsx
@@ -1,32 +1,65 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
+import {BrowserRouter, Link, Route, Routes} from "react-router-dom";
 
 function FrontPage() {
-  return (
-    <div>
-      <h1>Front Page</h1>
-      <div>
-        <Link to="/login">Login</Link>
-      </div>
-      <div>
-        <Link to="/profile">Profile</Link>
-      </div>
-    </div>
-  );
+    return (
+        <div>
+            <h1>Front Page</h1>
+            <div>
+                <Link to="/login">Login</Link>
+            </div>
+            <div>
+                <Link to="/profile">Profile</Link>
+            </div>
+        </div>
+    );
+}
+
+async function fetchJSON(url) {
+    const res = await fetch(url);
+    if (!res.ok) {
+        throw new Error(`failed ${res.status}`);
+    }
+    return await res.json();
+}
+
+function Login() {
+    const [redirectUrl, setRedirectUrl] = useState("");
+
+    useEffect(async () => {
+        const {authorization_endpoint} = await fetchJSON("https://accounts.google.com/.well-known/openid-configuration");
+        const parameters = {
+            response_type: "token",
+            client_id: "469230146682-2eat9sad9o7hbcs209at0n32j5d63h4j.apps.googleusercontent.com",
+            scope: "email profile",
+            redirect_uri: window.location.origin + "/login/callback"
+        };
+
+        setRedirectUrl(
+            authorization_endpoint + "?" + new URLSearchParams(parameters)
+        )
+    }, []);
+    return (
+        <div>
+            <h1>Login updated!</h1>
+            <a href={redirectUrl}>Do login</a>
+            <div>{redirectUrl}</div>
+        </div>
+    );
 }
 
 function Application() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path={"/"} element={<FrontPage />} />
-        <Route path={"/login"} element={<h1>Login</h1>} />
-        <Route path={"/login/callback"} element={<h1>Login callback</h1>} />
-        <Route path={"/profile"} element={<h1>Profile</h1>} />
-      </Routes>
-    </BrowserRouter>
-  );
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path={"/"} element={<FrontPage/>}/>
+                <Route path={"/login"} element={<Login/>}/>
+                <Route path={"/login/callback"} element={<h1>Login callback</h1>}/>
+                <Route path={"/profile"} element={<h1>Profile</h1>}/>
+            </Routes>
+        </BrowserRouter>
+    );
 }
 
-ReactDOM.render(<Application />, document.getElementById("app"));
+ReactDOM.render(<Application/>, document.getElementById("app"));
